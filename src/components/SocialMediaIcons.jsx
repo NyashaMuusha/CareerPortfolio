76bb1a789253eb117117340c3f7d97d55b1f34aa
Nyasha Ivy Peace <nyashamuusha@gmail.com>
2023-02-02 20:28:42 +0200

    first commit

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,30 @@
+const SocialMediaIcons = () => {
+  return (
+  <div className="flex justify-center md:justify-start my-10 gap-7">
+    <a className="hover:opacity-50 transition duration-500"
+    href='https://www.linkedin.com/in/nyasha-ivy-peace-muusha-279457a3/'
+    target='_blank'
+    rel='noreferrer'>
+      <img src='assets/linkedin.png' alt='linkedin'/>
+    </a>
+    <a className="hover:opacity-50 transition duration-500"
+    href='https://www.linkedin.com/in/nyasha-ivy-peace-muusha-279457a3/'
+    target='_blank'
+    rel='noreferrer'>
+      <img src='assets/instagram.png' alt='instagram'/>
+    </a>
+    <a className="hover:opacity-50 transition duration-500"
+    href='https://www.linkedin.com/in/nyasha-ivy-peace-muusha-279457a3/'
+    target='_blank'
+    rel='noreferrer'>
+      <img src='assets/facebook.png' alt='facebook'/>
+    </a>
+    <a className="hover:opacity-50 transition duration-500"
+    href='https://www.linkedin.com/in/nyasha-ivy-peace-muusha-279457a3/'
+    target='_blank'
+    rel='noreferrer'>
+      <img src='assets/twitter.png' alt='twitter'/>
+    </a>
+  </div>)
+}
+export default SocialMediaIcons;
